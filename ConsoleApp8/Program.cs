285f777fdc66dc5881b9f411ae5e50943525ae1a
acsili <ilyaovchinnikov4@gmail.com>
2024-02-16 22:58:09 +0400

    initial new branch

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 /*Сумма ряда (2n!)^n*/
+
+Console.WriteLine("Enter n: ");
+var n = int.Parse(Console.ReadLine()!);
+
+
+var ans = Enumerable.Range(1, n).Aggregate(0, (x, y) => x + (int)Math.Pow(2 * Factorial(y), y));
+if (ans > 0)
+    Console.WriteLine("Answer:\n{0}", ans);
+else
+    Console.WriteLine("Value is over int");
+
+
+static int Factorial(int n)
+{
+    if (n == 1) return 1;
+
+    return n * Factorial(n - 1);
+}
